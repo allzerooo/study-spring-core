1363177a9b205b21d69aa72627a2d4dde904a304
최다영 <dychoi.dev@gmail.com>
2020-11-19 19:27:13 +0900

    클라이언트가 인터페이스에만 의존하도록 변경

--- src/main/java/study/core/order/OrderServiceImpl.java
+++ src/main/java/study/core/order/OrderServiceImpl.java
@@ -1,7 +1,6 @@
 package study.core.order;
 
 import study.core.discount.DiscountPolicy;
-import study.core.discount.FixDiscountPolicy;
 import study.core.member.Member;
 import study.core.member.MemberRepository;
 import study.core.member.MemoryMemberRepository;
@@ -9,7 +8,7 @@ import study.core.member.MemoryMemberRepository;
 public class OrderServiceImpl implements OrderService {
 
     private final MemberRepository memberRepository = new MemoryMemberRepository();
-    private final DiscountPolicy discountPolicy = new FixDiscountPolicy();
+    private DiscountPolicy discountPolicy;
 
     @Override
     public Order createOrder(Long memberId, String itemName, int itemPrice) {
