db15dcbd2be8b05e9a5c885b13b12ffb01b33309
최다영 <dychoi.dev@gmail.com>
2020-11-30 20:42:35 +0900

    추가 : 싱글톤 패턴을 적용한 객체 사용 테스트

--- src/test/java/study/core/sigleton/SingletonTest.java
+++ src/test/java/study/core/sigleton/SingletonTest.java
@@ -7,7 +7,7 @@ import study.core.member.MemberService;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
-public class SingletonTest {
+class SingletonTest {
 
     @Test
     @DisplayName("스프링 없는 순수한 DI 컨테이너")
@@ -26,5 +26,17 @@ public class SingletonTest {
 
         //memberService1 != memberService2
         assertThat(memberService1).isNotSameAs(memberService2);
+    }
+
+    @Test
+    @DisplayName("싱글톤 패턴을 적용한 객체 사용")
+    void singletonServiceTest() {
+        SingletonService singletonService1 = SingletonService.getInstance();
+        SingletonService singletonService2 = SingletonService.getInstance();
+
+        System.out.println("singletonService1 = " + singletonService1);
+        System.out.println("singletonService2 = " + singletonService2);
+
+        assertThat(singletonService1).isSameAs(singletonService2);
     }
 }
